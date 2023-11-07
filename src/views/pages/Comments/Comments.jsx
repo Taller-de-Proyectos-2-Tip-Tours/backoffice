afe726116500b4b00398a208977c550c2bff8c56
Santiago39353535 <santiagomarinaro1@gmail.com>
2023-11-07 07:54:33 -0300

    agergado nombre del paseo a los comentarios

--- src/views/pages/Comments/Comments.jsx
+++ src/views/pages/Comments/Comments.jsx
@@ -16,6 +16,7 @@ import moment from 'moment/moment';
 import Pagination from 'react-bootstrap/Pagination';
 import { Rating } from '@mui/material';
 import Modal from 'react-bootstrap/Modal';
+import Container from 'react-bootstrap/Container';
 
 const Comments = () => {
     const navigate = useNavigate();
@@ -25,6 +26,7 @@ const Comments = () => {
     const [comments,setComents] = useState([])
     const [commentsToShow,setComentsToShow] = useState([])
     const [rating,setRating] = useState(null)
+    const [tour,setTour] = useState(null)
 
     const [page,setPage] = useState(0)
     const [pageSize,setPageSize] = useState(5)
@@ -34,8 +36,23 @@ const Comments = () => {
     const [modalMessage, setModalMessage] = useState(['']);
 
     useEffect(()=>{
+        if(id) getTour()
         if(id) getComments()
     },[id])
+
+    const getTour = () => {
+        apiClient.get(`/tours/${id}`,{headers:{'token':'admin'}})
+        .then((result)=>{
+            setLoading(false)
+            if(result) {
+                setTour(result)
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+            setLoading(false)
+        })
+    }
 
     const getComments = () => {
         apiClient.get(`/reviews/${id}`,{headers:{'token':'admin'}})
@@ -97,58 +114,70 @@ const Comments = () => {
 
     return(
         <>
-        {rating&&<Row style={{marginTop:12,marginBottom:12,alignItems:'center'}}>
-            <Col><h2>Valoraciones</h2></Col>
-            <Col><Rating defaultValue={rating} precision={0.5} readOnly /></Col>
-        </Row>}
-        <Row style={{justifyContent:'center'}}>
-            <h2>Reseñas</h2>
-            {commentsToShow&&commentsToShow.map((item,index)=>{
-                return <Card style={{paddingLeft:0,paddingRight:0,maxWidth:600,marginBottom:12}} className={item.state==='inactive'?'deactivated-comments':''} key={`${item?._id?.$oid}${index}`}>
-                    <Card.Title style={{color:'white',paddingLeft:8}} className={item.state==='inactive'?'cancel':'commemt'}><Row style={{marginTop:4}} ><Col>{item.userName}</Col><Col style={{fontSize:16}}>{moment(item.date).format('DD/MM/YYYY HH:ss')}</Col></Row></Card.Title>
-                    <Card.Body>
-                        <Row>{item.comment}</Row>
-                        <Row><span style={{justifyContent:'end',display:'flex',alignItems:'center'}}>{item.stars}<FontAwesomeIcon style={{color:'#caca03'}} icon={faStar}></FontAwesomeIcon></span></Row>
-                        {item.state!=='inactive'&&<Row><Col style={{textAlign:'end'}}><Button onClick={()=>deleteComment(item?._id?.$oid)} className="cancel">Desactivar Comentario</Button></Col></Row>}
-                    </Card.Body>
-                </Card>
-            })}
-        </Row>
-        {comments&&
-            <Pagination style={{justifyContent:'center'}}>
-                <Pagination.First onClick={()=>setPage(0)} />
-                <Pagination.Prev onClick={()=>{
-                    if(page-1>=0) {
-                        setPage(page-1)
+        <Container>
+            <Card>
+                <Card.Title style={{backgroundColor:'#4E598C',color:'white',paddingLeft:12}}>
+                    <Row>
+                        <Col>{tour?.name}</Col>
+                        <Col xs={3}>{tour?.guide?.name}</Col>
+                    </Row>
+                </Card.Title>
+                <Card.Body>
+                    {rating&&<Row style={{marginTop:12,marginBottom:12,alignItems:'center'}}>
+                        <Col><h2>Valoraciones</h2></Col>
+                        <Col><Rating defaultValue={rating} precision={0.5} readOnly /></Col>
+                    </Row>}
+                    <Row style={{justifyContent:'center'}}>
+                        <h2>Reseñas</h2>
+                        {commentsToShow&&commentsToShow.map((item,index)=>{
+                            return <Card style={{paddingLeft:0,paddingRight:0,maxWidth:600,marginBottom:12}} className={item.state==='inactive'?'deactivated-comments':''} key={`${item?._id?.$oid}${index}`}>
+                                <Card.Title style={{color:'white',paddingLeft:8}} className={item.state==='inactive'?'cancel':'commemt'}><Row style={{marginTop:4}} ><Col>{item.userName}</Col><Col style={{fontSize:16}}>{moment(item.date).format('DD/MM/YYYY HH:ss')}</Col></Row></Card.Title>
+                                <Card.Body>
+                                    <Row>{item.comment}</Row>
+                                    <Row><span style={{justifyContent:'end',display:'flex',alignItems:'center'}}>{item.stars}<FontAwesomeIcon style={{color:'#caca03'}} icon={faStar}></FontAwesomeIcon></span></Row>
+                                    {item.state!=='inactive'&&<Row><Col style={{textAlign:'end'}}><Button onClick={()=>deleteComment(item?._id?.$oid)} className="cancel">Desactivar Comentario</Button></Col></Row>}
+                                </Card.Body>
+                            </Card>
+                        })}
+                    </Row>
+                    {comments&&
+                        <Pagination style={{justifyContent:'center'}}>
+                            <Pagination.First onClick={()=>setPage(0)} />
+                            <Pagination.Prev onClick={()=>{
+                                if(page-1>=0) {
+                                    setPage(page-1)
+                                }
+                            }}/>
+                            {getPaginationItems()}
+                            <Pagination.Next  onClick={()=>{
+                                if(page+1<cantPages) {
+                                    setPage(page+1)
+                                }
+                            }}/>
+                            <Pagination.Last onClick={()=>setPage(cantPages-1)}/>
+                        </Pagination>
                     }
-                }}/>
-                {getPaginationItems()}
-                <Pagination.Next  onClick={()=>{
-                    if(page+1<cantPages) {
-                        setPage(page+1)
-                    }
-                }}/>
-                <Pagination.Last onClick={()=>setPage(cantPages-1)}/>
-            </Pagination>
-        }
-        {loading&&<Loader></Loader>}
-        {modal&&<div
-                className="modal show modal-full-page"
-            >
-            <Modal.Dialog>
-                <Modal.Header>
-                    <Modal.Title>Edicion del Paseo</Modal.Title>
-                </Modal.Header>
+                    {loading&&<Loader></Loader>}
+                    {modal&&<div
+                            className="modal show modal-full-page"
+                        >
+                        <Modal.Dialog>
+                            <Modal.Header>
+                                <Modal.Title>Edicion del Paseo</Modal.Title>
+                            </Modal.Header>
 
-                <Modal.Body>
-                    {modalMessage.map((item)=><p>{item}</p>)}
-                </Modal.Body>
+                            <Modal.Body>
+                                {modalMessage.map((item)=><p>{item}</p>)}
+                            </Modal.Body>
 
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={()=>showModal(false)}>Cerrar</Button>
-                </Modal.Footer>
-            </Modal.Dialog>
-            </div>}
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={()=>showModal(false)}>Cerrar</Button>
+                            </Modal.Footer>
+                        </Modal.Dialog>
+                        </div>}
+                </Card.Body>
+            </Card>
+        </Container>
         </>
     )
 }
