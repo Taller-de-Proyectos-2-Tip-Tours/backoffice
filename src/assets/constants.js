e2da4d03d0c5775f782a232adcd5ef0053634e38
Santiago39353535 <santiagomarinaro1@gmail.com>
2023-11-24 19:34:04 -0300

    agregado de token backoffice

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -14,5 +14,6 @@ module.exports = {
         'Francés',
         'Italiano'   
     ],
-    MAP_BOX_KEY:'pk.eyJ1Ijoic2FudGlhZ29tYXJpbmFybyIsImEiOiJjbG1waDdrbTMwdGZ4MmtxZ2wxc2ZzZnFvIn0.kZ-SnEEKwxIIrGQY995I8w'
+    MAP_BOX_KEY:'pk.eyJ1Ijoic2FudGlhZ29tYXJpbmFybyIsImEiOiJjbG1waDdrbTMwdGZ4MmtxZ2wxc2ZzZnFvIn0.kZ-SnEEKwxIIrGQY995I8w',
+    TOKEN:'8a39e109904c2a9bc907203b6490b61c'
 }
