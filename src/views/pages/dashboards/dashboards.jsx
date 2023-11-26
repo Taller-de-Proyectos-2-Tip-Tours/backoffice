5755f9563b501b04a3c8661e68b973f76337cc38
Santiago39353535 <santiagomarinaro1@gmail.com>
2023-11-26 19:41:24 -0300

    fix

--- src/views/pages/dashboards/dashboards.jsx
+++ src/views/pages/dashboards/dashboards.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import './dashboards.css';
 import MapDashboards from './mapDashboard/mapDashboard';
 import AppEvolution from './appEvolution/appEvolution';
